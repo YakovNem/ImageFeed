7e4d1203eabb403557b156a35401c64556ea3474
YakovNem <yasha.nem3712@gmail.com>
2022-11-27 20:46:27 +0300

    test

--- ImageFeed/AppDelegate.swift
+++ ImageFeed/AppDelegate.swift
@@ -26,7 +26,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     }
 
     func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
-        // Called when the user discards a scene session.
+        // Called when the user discards a scene session
     }
 
 
