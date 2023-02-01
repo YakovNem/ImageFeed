e9450c206615d81aab03435a90b6e85f36321d17
YakovNem <yasha.nem3712@gmail.com>
2023-02-01 19:39:58 +0300

    Sprint 8

--- ImageFeed/ImagesList/ImagesListCell.swift
+++ ImageFeed/ImagesList/ImagesListCell.swift
@@ -8,5 +8,8 @@ import UIKit
 import Foundation
 
 final class ImagesListCell: UITableViewCell {
-    
+    static let reuseIdentifier = "ImagesListCell"
+    @IBOutlet weak var cellImage: UIImageView!
+    @IBOutlet weak var likeButton: UIButton!
+    @IBOutlet weak var dateLabel: UILabel!
 }
